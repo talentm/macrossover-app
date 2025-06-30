99b3c71a4f1a8f2d93458e290b0125a1fe853a82
Talent Mataba <matabatalent@gmail.com>
2025-06-30 13:07:35 +0200

    wrapped macrossover

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,9 @@
+import { useColorScheme } from '@/hooks/useColorScheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import HomeScreen from './(tabs)/index';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -19,10 +18,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <HomeScreen />
       <StatusBar style="auto" />
     </ThemeProvider>
   );
